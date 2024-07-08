0f4f9cb43cb5fce90d6ecfb34fa0d5553b56c78d
Stephanie Metieh <stephaniemetieh@gmail.com>
2024-07-08 11:30:10 +0100

    corrected window location

--- cart-page/app.js
+++ cart-page/app.js
@@ -94,6 +94,7 @@ initApp();
 
 // Event Listeners
 container.addEventListener("click", function (e) {
+  console.log(e.target);
   if (
     !e.target.classList.contains("minus") ||
     !e.target.classList.contains("plus")
