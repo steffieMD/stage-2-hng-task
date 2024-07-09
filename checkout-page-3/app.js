320bc095379bdf0900305e4d6a7c97a55efee5df
Stephanie Metieh <stephaniemetieh@gmail.com>
2024-07-09 19:25:51 +0100

    created final checkout page

---
+++ checkout-page-3/app.js
@@ -0,0 +1,27 @@
+const cartIcon = document.querySelector(".cart-icon");
+const btnCheckOut = document.querySelector(".checkout-btn");
+const btnMen = document.querySelector(".men");
+const shippingHeader = document.querySelector(".shipping-header");
+const homePage = document.querySelector(".homePage");
+console.log(homePage);
+
+btnMen.addEventListener("click", function () {
+  window.location = "/index.html";
+});
+
+cartIcon.addEventListener("click", function () {
+  window.location = "/cart-page/cart.html";
+});
+
+btnCheckOut.addEventListener("click", function () {
+  // window.location = "checkout-page-2/checkout.html";
+  alert("STILL COOKING ;)");
+});
+
+shippingHeader.addEventListener("click", function () {
+  window.location = "/checkout-page/checkout.html";
+});
+
+homePage.addEventListener("click", function () {
+  window.location = "/index.html";
+});
